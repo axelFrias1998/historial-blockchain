adca6279b42b15b7580404c756df2b7a6a2fe930
Axel Frías Hernández <axel.frias257@gmail.com>
2021-05-12 21:20:43 -0500

    Aplicación de contexto de de datos en clase Program/Main

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using historial_blockchain.Contexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,16 @@ namespace historial_blockchain
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var webHost = CreateHostBuilder(args).Build();
+ 
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        
+            webHost.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
