d51ce365cb90721c73d1a0c52f69bf33e8dc8701
Axel Frías Hernández <axel.frias257@gmail.com>
2021-05-18 12:27:07 -0500

    Cambio en HospitalsDTO. Se quitan datos de los objetos internos

--- Models/DTOs/HospitalsDTO.cs
+++ Models/DTOs/HospitalsDTO.cs
@@ -11,15 +11,7 @@ namespace historial_blockchain.Models.DTOs
         public string PhoneNumer { get; set; }
 
         public DateTime RegisterDate { get; set; }
-
-        public string Nombre { get; set; }
-
-        public string Apellido { get; set; }
-
-        public string Email { get; set; }
-
-        public string Type { get; set; }
-
+        
         public CreatedUserDTO Admin { get; set; }
 
         public ServicesCatalogDTO ServicesCatalog { get; set; }
