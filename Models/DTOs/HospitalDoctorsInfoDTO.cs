964c6484c1c04220857492ce3a5038f1df3dc46c
Axel Frías Hernández <axel.frias257@gmail.com>
2021-06-08 13:05:19 -0500

    HospitalDoctorsInfoDTO

---
+++ Models/DTOs/HospitalDoctorsInfoDTO.cs
@@ -0,0 +1,17 @@
+namespace historial_blockchain.Models.DTOs
+{
+    public class HospitalDoctorsInfoDTO
+    {
+        public string DoctorId { get; set; }
+        
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Email { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Especialidad { get; set; }
+    }
+}
